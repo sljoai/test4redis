805b384309a0e2ef69c10878f45971396c852a46
songlaijiang <99026795@qq.com>
2018-10-30 22:51:06 +0800

    测试redis使用的类

---
+++ test/src/main/java/com/song/cn/test/Test4Redis.java
@@ -0,0 +1,40 @@
+package com.song.cn.test;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test4Redis {
+    private static JedisCluster jedisCluster;
+    private static Jedis jedis = new Jedis("");
+    static {
+        Set<HostAndPort> nodes = new HashSet<HostAndPort>();
+        nodes.add(new HostAndPort("uplooking01", 7000));
+        nodes.add(new HostAndPort("uplooking01", 7001));
+        nodes.add(new HostAndPort("uplooking01", 7002));
+        nodes.add(new HostAndPort("uplooking01", 7003));
+        nodes.add(new HostAndPort("uplooking01", 7004));
+        nodes.add(new HostAndPort("uplooking01", 7005));
+        jedisCluster = new JedisCluster(nodes);//得到的是redis的集群模式
+    }
+
+    /**
+     * 提供了Jedis的对象
+     * @return
+     */
+    public static JedisCluster getJedis() {
+        return jedisCluster;
+    }
+
+    /**
+     * 资源释放
+     * @param jedis
+     */
+    public static void returnJedis(JedisCluster jedis) {
+
+    }
+
+}
